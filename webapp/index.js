d05947beef42cdaf2890b90c14f324594f5db3c5
Samer Zukari <samer.zukari@si-pro.de>
2025-05-13 12:23:00 +0000

    Meine Änderungen z. B. UI5-Konfiguration oder neue Komponenten

---
+++ webapp/index.js
@@ -0,0 +1,17 @@
+sap.ui.define(
+    ["sap/ui/core/ComponentContainer"],
+    function (ComponentContainer) {
+      "use strict";
+  
+      var oContainer = new ComponentContainer({
+        id: "container",
+        name: "sap.training.exc",
+        manifest: true,
+        async: true,
+        settings: {
+          id: "sap.training.exc",
+        },
+      });
+      oContainer.placeAt("content");
+    }
+  );
